108778b72e74d6217c594bb7bd32834d340b0e52
Guilherme Gomes do Braz <ggbraz@gmail.com>
2021-08-09 15:29:33 -0300

    Inicio Projeto Personagens do Jogo

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace POO_JOGOS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
